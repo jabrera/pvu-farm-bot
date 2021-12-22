143545af163066bfd60f3a1931fd590e17ab1bbc
Juvar Abrera <me@juvarabrera.com>
2021-12-22 18:40:25 +0800

    Update index.js
    Added request.
    Added logs
    Buy water or scarecrow if insufficient.

--- index.js
+++ index.js
@@ -10,53 +10,18 @@ var App = {
             WINTER: "winter",
         },
 
-        BACK_ICON: "/_nuxt/img/btn-back.c2ee022.svg",
-        SHOP_ICON: "/_nuxt/img/shop@3x.6f5bc8a.png",
-        INVENTORY_ICON: "/_nuxt/img/assets@3x.b02c22d.png",
-        WORLD_TREE_ICON: "/_nuxt/img/world-tree.399e2f8.png",
-        SF_SAPLING_ICON: "/_nuxt/img/sapling.fec9ca6.svg",
-        SF_MAMA_ICON: "/_nuxt/img/mama.183c7fe.svg",
-
-        INVENTORY__SF_SAPLING_ICON: "/_nuxt/img/sapling.ec87981.svg",
-        INVENTORY__SF_MAMA_ICON: "/_nuxt/img/mama.183c7fe.svg",
-        
-        WATER_ICON: "/_nuxt/img/water@3x.22b98e2.png",
-        CROW_ICON: "/_nuxt/img/scarecrow@3x.50e7720.png",
-        SMALL_POT_ICON: "/_nuxt/img/small pot@3x.ff8750b.png",
-        BIG_POT_ICON: "/_nuxt/img/big pot@3x.430c95a.png",
-        
-        FARM_INACTIVE_ICON: "/_nuxt/img/farm-inactive.0b47685.svg",
-        SEED_INACTIVE_ICON: "/_nuxt/img/seed-inactive.ed4e273.png",
-        SUNFLOWER_INACTIVE_ICON: "/_nuxt/img/sunflower-inactive-new.6a7dd8e.png",
-        SHOP__TOOLS_INACTIVE_ICON: "/_nuxt/img/tool-inactive-new.1f58383.png",
-        PLANT__INACTIVE_ICON: "/_nuxt/img/plant-inactive-new.3472926.png",
-        MOTHER_TREE_INACTIVE_ICON: "/_nuxt/img/mother-tree-inactive-new.9a2844d.png",
-        CLAIM_SEED_INACTIVE_ICON: "/_nuxt/img/claim-seed-inactive.1a04446.png",
-
-        PLANT_ICON: "/_nuxt/img/plant.a020463.svg",
-        MOTHER_TREE_ICON: "/_nuxt/img/mtree.ca90f28.svg",
-
-        QR_CODE_ICON: "/_nuxt/img/qr-code.407e668.svg",
-
-        WATER: "water",
-        SMALL_POT: "small pot",
-        BIG_POT: "big pot",
-        GREENHOUSE: "greenhouse",
-        SCARECROW: "scarecrow",
-        SF_SAPLING: "sapling",
-        SF_MAMA: "mama",
-        
-        URL: {
-            WEATHER_TODAY: "https://backend-farm.plantvsundead.com/weather-today",
-            FARMING_STATS: "https://backend-farm.plantvsundead.com/farming-stats",
-            GET_FARMING_PLANTS: "https://backend-farm.plantvsundead.com/v2/farms/?offset=0&limit=10",
-            HARVEST_PLANT: "https://backend-farm.plantvsundead.com/farms/{{id}}/harvest",
-            APPLY_TOOL: "https://backend-farm.plantvsundead.com/farms/apply-tool",
-            MY_TOOLS: "https://backend-farm.plantvsundead.com/my-tools",
-            AVAILABLE_TOOLS: "https://backend-farm.plantvsundead.com/available-tools",
-            SUNFLOWERS: "https://backend-farm.plantvsundead.com/sunflowers",
-            BUY_TOOLS: "https://backend-farm.plantvsundead.com/buy-tools",
-            FARMING_STATS: "https://backend-farm.plantvsundead.com/farming-stats",
+        ROOT_URL: "https://backend-farm.plantvsundead.com",
+        API: {
+            WEATHER_TODAY: "/weather-today",
+            FARMING_STATS: "/farming-stats",
+            FARMING_PLANTS: "/v2/farms/?offset=0&limit=10",
+            HARVEST_PLANT: "/farms/{{id}}/harvest",
+            APPLY_TOOL: "/farms/apply-tool",
+            MY_TOOLS: "/my-tools",
+            AVAILABLE_TOOLS: "/available-tools",
+            SUNFLOWERS: "/sunflowers",
+            BUY_TOOLS: "/buy-tools",
+            FARMING_STATS: "/farming-stats",
         },
         
         FARMING_STAGE: {
@@ -71,11 +36,54 @@ var App = {
             SCARECROW: "4"   
         }
     },
+    Request: {
+        Method: {
+            GET: "get",
+            POST: "post"
+        },
+        getHeaderOptions: function(method, data = null) {
+            var options = {
+                method: method,
+                headers: new Headers({
+                    'authorization': 'Bearer Token: ' + localStorage.getItem("token"),
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                })
+            };
+            if(data != null) {
+                options.body = JSON.stringify(data)
+            }
+            return options;
+        },
+        get: async function(url, data = null) {
+            return new Promise(async function(resolve) {
+                resolve(await App.Request.send(url, App.Request.Method.GET, data));
+            });
+        },
+        post: async function(url, data = null) {
+            return new Promise(async function(resolve) {
+                resolve(await App.Request.send(url, App.Request.Method.POST, data));
+            });
+        },
+        send: async function(url, method, data = null) {
+            return new Promise(async function(resolve) {
+                App.Utility.log(`${method} ${url}`);
+                if(data != null) {
+                    App.Utility.log(`with data ${JSON.stringify(data)}`);
+                }
+                const request = await fetch(App.Constant.ROOT_URL + url, App.Request.getHeaderOptions(method, data));
+                const response = await request.json();
+                App.Utility.log(JSON.stringify(response));
+                resolve(response);
+            });
+        },
+    },
     Utility: {
         getRandomArbitrary: function(min, max) {
             return Math.random() * (max - min) + min;
         },
         timeout: function(ms = App.Constant.TIMEOUT) {
+            console.log("timeout for " + ms);
             return new Promise(resolve => setTimeout(resolve, ms));
         },
         log: function(msg) {
@@ -83,30 +91,6 @@ var App = {
                 var date = new Date();
                 console.log("[INFO] " + date.toLocaleDateString() + " " + date.toLocaleTimeString() + " -- " + msg);
             }
-        },
-        getIcon: function(tool) {
-            icon = "";
-            switch (tool) {
-                case App.Constant.WATER:
-                    icon = App.Constant.WATER_ICON;
-                    break;
-                case App.Constant.SCARECROW:
-                    icon = App.Constant.CROW_ICON;
-                    break;
-                case App.Constant.SMALL_POT:
-                    icon = App.Constant.SMALL_POT_ICON;
-                    break;
-                case App.Constant.BIG_POT:
-                    icon = App.Constant.BIG_POT_ICON;
-                    break;
-                case App.Constant.SF_SAPLING:
-                    icon = App.Constant.SF_SAPLING_ICON;
-                    break;
-                case App.Constant.SF_MAMA:
-                    icon = App.Constant.SF_MAMA_ICON;
-                    break;
-            }
-            return icon;
         },
     },
     General: {
@@ -129,9 +113,8 @@ var App = {
         bnb: 0,
         get: function() {
             return new Promise(async function(resolve) {
-                const r1 = await fetch(App.Constant.URL.FARMING_STATS, App.Request.getHeaderOptions(App.Request.Method.GET));
-                const j1 = await r1.json(); 
-                App.Balance.le = j1.data.leWallet;
+                const farming_stats = await App.Request.get(App.Constant.API.FARMING_STATS);
+                App.Balance.le = farming_stats.data.leWallet;
                 resolve();
             });
         }
@@ -142,10 +125,9 @@ var App = {
         WATER: 0,
         init: function() {
             return new Promise(async function(resolve) {
-                const r1 = await fetch(App.Constant.URL.MY_TOOLS, App.Request.getHeaderOptions(App.Request.Method.GET));
-                const j1 = await r1.json(); 
-                for(var tool in j1.data) {
-                    tool = j1.data[tool];
+                const my_tools = await App.Request.get(App.Constant.API.MY_TOOLS);
+                for(var tool in my_tools.data) {
+                    tool = my_tools.data[tool];
                     if(App.Tools.hasOwnProperty(tool.type)) {
                         App.Tools[tool.type] = tool.usages;
                     }
@@ -158,10 +140,8 @@ var App = {
         go: function() {
             return new Promise(async function(resolve) {
                 // does nothing but needs to request
-                const r1 = await fetch(App.Constant.URL.AVAILABLE_TOOLS, App.Request.getHeaderOptions(App.Request.Method.GET));
-                const j1 = await r1.json();
-                const r2 = await fetch(App.Constant.URL.SUNFLOWERS, App.Request.getHeaderOptions(App.Request.Method.GET));
-                const j2 = await r2.json();
+                await App.Request.get(App.Constant.API.AVAILABLE_TOOLS);
+                await App.Request.get(App.Constant.API.SUNFLOWERS);
                 await App.Utility.timeout();
                 resolve();
             });
@@ -169,44 +149,23 @@ var App = {
         buy: function(toolId, amount) {
             return new Promise(async function(resolve) {
                 await App.Shop.go();
-                const r2 = await fetch(App.Constant.URL.BUY_TOOLS, App.Request.getHeaderOptions(App.Request.Method.POST, {
+                await App.Request.post(App.Constant.API.BUY_TOOLS, {
                     amount: amount,
                     toolId: toolId
-                }));
-                const j2 = await r2.json();
+                });
                 await App.Farm.getStats();
                 await App.Utility.timeout();
                 await App.Farm.go();
+                resolve();
             });
-        }
-    },
-    Request: {
-        Method: {
-            GET: "get",
-            POST: "post"
-        },
-        getHeaderOptions: function(method, data = null) {
-            var options = {
-                method: method,
-                headers: new Headers({
-                    'authorization': 'Bearer Token: ' + localStorage.getItem("token"),
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                })
-            };
-            if(data != null) {
-                options.body = JSON.stringify(data)
-            }
-            return options;
         }
     },
     Weather: {
         data: {},
         get: function() {
             return new Promise(async function(resolve) {
-                const r1 = await fetch(App.Constant.URL.WEATHER_TODAY, App.Request.getHeaderOptions(App.Request.Method.GET));
-                const j1 = await r1.json();
-                App.Weather.data = j1.data;
+                const weather = await App.Request.get(App.Constant.API.WEATHER_TODAY);
+                App.Weather.data = weather.data;
                 resolve();
             });
         }
@@ -221,19 +180,23 @@ var App = {
                     App.Balance.le += r.data.amount;
                 }
                 if(plant.hasCrow) {
-                    const scarecrow_request = await fetch(App.Constant.URL.APPLY_TOOL, App.Request.getHeaderOptions(App.Request.Method.POST, {
+                    if(App.Tools.SCARECROW == 0) {
+                        await App.Shop.buy(App.Constant.TOOL.SCARECROW,1);
+                    }
+                    await App.Request.post(App.Constant.API.APPLY_TOOL, {
                         farmId: plant._id,
                         toolId: App.Constant.TOOL.SCARECROW
-                    }));
-                    const scarecrow_response = await scarecrow_request.json();
+                    });
                     await App.Utility.timeout();
                 }
                 if(plant.needWater) {
-                    const water_request = await fetch(App.Constant.URL.APPLY_TOOL, App.Request.getHeaderOptions(App.Request.Method.POST, {
+                    if(App.Tools.WATER < 2) {
+                        await App.Shop.buy(App.Constant.TOOL.WATER,1);
+                    }
+                    await App.Request.post(App.Constant.API.APPLY_TOOL, {
                         farmId: plant._id,
                         toolId: App.Constant.TOOL.WATER
-                    }));
-                    const water_response = await water_request.json();
+                    });
                     await App.Utility.timeout();
                 }
                 for(var tool in plant.activeTools) {
@@ -243,11 +206,10 @@ var App = {
                             if(App.Tools[tool.type] == 0) {
                                 await App.Shop.buy(App.Constant.TOOL.POT,1);
                             }
-                            const pot_request = await fetch(App.Constant.URL.APPLY_TOOL, App.Request.getHeaderOptions(App.Request.Method.POST, {
+                            await App.Request.post(App.Constant.API.APPLY_TOOL, {
                                 farmId: plant._id,
                                 toolId: App.Constant.TOOL.POT
-                            }));
-                            const pot_response = await pot_request.json();
+                            });
                             await App.Utility.timeout();
                         }
                     }
@@ -265,9 +227,7 @@ var App = {
         },
         getStats: function() {
             return new Promise(async function(resolve) {
-                const r1 = await fetch(App.Constant.URL.GET_FARMING_PLANTS, App.Request.getHeaderOptions(App.Request.Method.GET));
-                const j1 = await r1.json();
-                resolve();
+                resolve(await App.Request.get(App.Constant.API.FARMING_STATS));
             });
         },
         Land: {
@@ -275,16 +235,12 @@ var App = {
         Plant: {
             getFarming: function() {
                 return new Promise(async function(resolve) {
-                    const r1 = await fetch(App.Constant.URL.GET_FARMING_PLANTS, App.Request.getHeaderOptions(App.Request.Method.GET));
-                    const j1 = await r1.json(); 
-                    resolve(j1);
+                    resolve(await App.Request.get(App.Constant.API.FARMING_PLANTS));
                 });
             },
             harvest: function(plant) {
                 return new Promise(async function(resolve) {
-                    const r1 = await fetch(App.Constant.URL.HARVEST_PLANT.replace("{{id}}", plant._id), App.Request.getHeaderOptions(App.Request.Method.POST));
-                    const j1 = await r1.json(); 
-                    resolve(j1);
+                    resolve(await App.Request.post(App.Constant.API.HARVEST_PLANT.replace("{{id}}", plant._id)));
                 });
             }
         },
