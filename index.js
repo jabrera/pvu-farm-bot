3a5b8baae3686ec79f44494cd434ed4fa04debdb
Juvar Abrera <me@juvarabrera.com>
2021-12-22 18:06:35 +0800

    Added functionalities
    Get farm
    Harvest Plants
    Water plants
    Scare crow in plants
    Add pots
    If plant pot is less than 2, apply pot.
    If pot tool is insufficiency, buy pot then apply.

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-
 var App = {
     Constant: {
         DEVELOPMENT: true,
         TIMEOUT: 1000,
         SHORT_TIMEOUT: 500,
+        Weathers: {
+            SPRING: "spring",
+            SUMMER: "summer",
+            AUTUMN: "autumn",
+            WINTER: "winter",
+        },
 
         BACK_ICON: "/_nuxt/img/btn-back.c2ee022.svg",
         SHOP_ICON: "/_nuxt/img/shop@3x.6f5bc8a.png",
@@ -40,12 +45,37 @@ var App = {
         SCARECROW: "scarecrow",
         SF_SAPLING: "sapling",
         SF_MAMA: "mama",
+        
+        URL: {
+            WEATHER_TODAY: "https://backend-farm.plantvsundead.com/weather-today",
+            FARMING_STATS: "https://backend-farm.plantvsundead.com/farming-stats",
+            GET_FARMING_PLANTS: "https://backend-farm.plantvsundead.com/v2/farms/?offset=0&limit=10",
+            HARVEST_PLANT: "https://backend-farm.plantvsundead.com/farms/{{id}}/harvest",
+            APPLY_TOOL: "https://backend-farm.plantvsundead.com/farms/apply-tool",
+            MY_TOOLS: "https://backend-farm.plantvsundead.com/my-tools",
+            AVAILABLE_TOOLS: "https://backend-farm.plantvsundead.com/available-tools",
+            SUNFLOWERS: "https://backend-farm.plantvsundead.com/sunflowers",
+            BUY_TOOLS: "https://backend-farm.plantvsundead.com/buy-tools",
+            FARMING_STATS: "https://backend-farm.plantvsundead.com/farming-stats",
+        },
+        
+        FARMING_STAGE: {
+            PAUSED: "paused",
+            FARMING: "farming",
+            CANCELLED: "cancelled",
+        },
+        
+        TOOL: {
+            POT: "1",
+            WATER: "3",
+            SCARECROW: "4"   
+        }
     },
     Utility: {
         getRandomArbitrary: function(min, max) {
             return Math.random() * (max - min) + min;
         },
-        timeout: function(ms) {
+        timeout: function(ms = App.Constant.TIMEOUT) {
             return new Promise(resolve => setTimeout(resolve, ms));
         },
         log: function(msg) {
@@ -93,25 +123,177 @@ var App = {
         CLAIM_SEED: 0,
     },
     Balance: {
-        LE: 0,
-        Sapling: 0,
-        PVU: 0,
-        BNB: 0,
+        le: 0,
+        sapling: 0,
+        pvu: 0,
+        bnb: 0,
+        get: function() {
+            return new Promise(async function(resolve) {
+                const r1 = await fetch(App.Constant.URL.FARMING_STATS, App.Request.getHeaderOptions(App.Request.Method.GET));
+                const j1 = await r1.json(); 
+                App.Balance.le = j1.data.leWallet;
+                resolve();
+            });
+        }
     },
     Tools: {
+        POT: 0,
+        SCARECROW: 0,
+        WATER: 0,
+        init: function() {
+            return new Promise(async function(resolve) {
+                const r1 = await fetch(App.Constant.URL.MY_TOOLS, App.Request.getHeaderOptions(App.Request.Method.GET));
+                const j1 = await r1.json(); 
+                for(var tool in j1.data) {
+                    tool = j1.data[tool];
+                    if(App.Tools.hasOwnProperty(tool.type)) {
+                        App.Tools[tool.type] = tool.usages;
+                    }
+                }
+                resolve();
+            });
+        },
     },
     Shop: {
+        go: function() {
+            return new Promise(async function(resolve) {
+                // does nothing but needs to request
+                const r1 = await fetch(App.Constant.URL.AVAILABLE_TOOLS, App.Request.getHeaderOptions(App.Request.Method.GET));
+                const j1 = await r1.json();
+                const r2 = await fetch(App.Constant.URL.SUNFLOWERS, App.Request.getHeaderOptions(App.Request.Method.GET));
+                const j2 = await r2.json();
+                await App.Utility.timeout();
+                resolve();
+            });
+        },
+        buy: function(toolId, amount) {
+            return new Promise(async function(resolve) {
+                await App.Shop.go();
+                const r2 = await fetch(App.Constant.URL.BUY_TOOLS, App.Request.getHeaderOptions(App.Request.Method.POST, {
+                    amount: amount,
+                    toolId: toolId
+                }));
+                const j2 = await r2.json();
+                await App.Farm.getStats();
+                await App.Utility.timeout();
+                await App.Farm.go();
+            });
+        }
+    },
+    Request: {
+        Method: {
+            GET: "get",
+            POST: "post"
+        },
+        getHeaderOptions: function(method, data = null) {
+            var options = {
+                method: method,
+                headers: new Headers({
+                    'authorization': 'Bearer Token: ' + localStorage.getItem("token"),
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                })
+            };
+            if(data != null) {
+                options.body = JSON.stringify(data)
+            }
+            return options;
+        }
+    },
+    Weather: {
+        data: {},
+        get: function() {
+            return new Promise(async function(resolve) {
+                const r1 = await fetch(App.Constant.URL.WEATHER_TODAY, App.Request.getHeaderOptions(App.Request.Method.GET));
+                const j1 = await r1.json();
+                App.Weather.data = j1.data;
+                resolve();
+            });
+        }
     },
     Farm: {
+        init: async function() {
+            var plants = await App.Farm.Plant.getFarming();
+            for (var plant in plants.data) {
+                plant = plants.data[plant];
+                if(plant.stage == App.Constant.FARMING_STAGE.CANCELLED && plant.totalHarvest > 0) {
+                    var r = App.Farm.Plant.harvest(plant);
+                    App.Balance.le += r.data.amount;
+                }
+                if(plant.hasCrow) {
+                    const scarecrow_request = await fetch(App.Constant.URL.APPLY_TOOL, App.Request.getHeaderOptions(App.Request.Method.POST, {
+                        farmId: plant._id,
+                        toolId: App.Constant.TOOL.SCARECROW
+                    }));
+                    const scarecrow_response = await scarecrow_request.json();
+                    await App.Utility.timeout();
+                }
+                if(plant.needWater) {
+                    const water_request = await fetch(App.Constant.URL.APPLY_TOOL, App.Request.getHeaderOptions(App.Request.Method.POST, {
+                        farmId: plant._id,
+                        toolId: App.Constant.TOOL.WATER
+                    }));
+                    const water_response = await water_request.json();
+                    await App.Utility.timeout();
+                }
+                for(var tool in plant.activeTools) {
+                    tool = plant.activeTools[tool];
+                    if(tool.id == App.Constant.TOOL.POT) {
+                        if(tool.count < 2) {
+                            if(App.Tools[tool.type] == 0) {
+                                await App.Shop.buy(App.Constant.TOOL.POT,1);
+                            }
+                            const pot_request = await fetch(App.Constant.URL.APPLY_TOOL, App.Request.getHeaderOptions(App.Request.Method.POST, {
+                                farmId: plant._id,
+                                toolId: App.Constant.TOOL.POT
+                            }));
+                            const pot_response = await pot_request.json();
+                            await App.Utility.timeout();
+                        }
+                    }
+                }
+            }
+        },
+        go: function() {
+            return new Promise(async function(resolve) {
+                await App.Farm.getStats();
+                await App.Tools.init();
+                await App.Farm.Plant.getFarming();
+                await App.Utility.timeout();
+                resolve();
+            });
+        },
+        getStats: function() {
+            return new Promise(async function(resolve) {
+                const r1 = await fetch(App.Constant.URL.GET_FARMING_PLANTS, App.Request.getHeaderOptions(App.Request.Method.GET));
+                const j1 = await r1.json();
+                resolve();
+            });
+        },
         Land: {
         },
         Plant: {
+            getFarming: function() {
+                return new Promise(async function(resolve) {
+                    const r1 = await fetch(App.Constant.URL.GET_FARMING_PLANTS, App.Request.getHeaderOptions(App.Request.Method.GET));
+                    const j1 = await r1.json(); 
+                    resolve(j1);
+                });
+            },
+            harvest: function(plant) {
+                return new Promise(async function(resolve) {
+                    const r1 = await fetch(App.Constant.URL.HARVEST_PLANT.replace("{{id}}", plant._id), App.Request.getHeaderOptions(App.Request.Method.POST));
+                    const j1 = await r1.json(); 
+                    resolve(j1);
+                });
+            }
         },
     },
     init: async function() {
         var startDate = new Date();
         App.Utility.log("Initializing bot...");
-        
+        await App.Tools.init();
+        await App.Farm.init();
         var duration = (new Date() - startDate) / 1000;
         App.Utility.log("Bot finished in " + duration + " seconds.");
     }
