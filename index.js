0e120c40bf199f27db8bdbadbc5fdc644f11e6b8
Juvar Abrera <me@juvarabrera.com>
2022-01-15 10:42:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -636,7 +636,7 @@ var App = {
                 
                 if(totalAvailable != 0) {
                     App.Utility.log(`\tHas ${totalAvailable} free slots!`);
-                    // var selectedFarm = free_slots.data.farm[0];
+                    var selectedFarm = free_slots.data.farm[0];
                     if(plant_available > 0 && !skipPlantLots) {
                         var plants = await App.Farm.Plant.getMyPlants();
                         var selectedPlant = null;
@@ -661,7 +661,7 @@ var App = {
                                     if(App.Weather.data.allowedPlants.includes(plant.seasonType) && plant.rate.hours < diffHours && plant.usages != 0) {
                                         found = true;
                                         App.Utility.log(`\t\tPlanting sunflower sapling.`);
-                                        await App.Farm.Plant.add("0", plant.sunflowerId);
+                                        await App.Farm.Plant.add(selectedFarm, "0", plant.sunflowerId);
                                         App.Utility.log(`\t\tSunflower sapling planted!`);
                                     } else {
                                         break;
@@ -671,7 +671,7 @@ var App = {
                             if(!found) skipPlantLots =  true;
                         } else {
                             App.Utility.log(`\t\tPlanted ${selectedPlant.plantId} (${selectedPlant.plant.stats.type})!`);
-                            await App.Farm.Plant.add("0", selectedPlant);
+                            await App.Farm.Plant.add(selectedFarm, "0", selectedPlant);
                         }
                         plant_available--;
                     } else if(motherTree_available > 0 && !skipMotherTreeLots) {
@@ -697,7 +697,7 @@ var App = {
                                 if(plant.type == App.Constant.SUNFLOWER.MAMA) {
                                     if(App.Weather.data.allowedPlants.includes(plant.seasonType) && plant.rate.hours < diffHours && plant.usages != 0) {
                                         App.Utility.log(`\t\tPlanting sunflower mama.`);
-                                        await App.Farm.Plant.add("0", plant.sunflowerId);
+                                        await App.Farm.Plant.add(selectedFarm, "0", plant.sunflowerId);
                                         App.Utility.log(`\t\tSunflower mama planted!`);
                                     } else {
                                         break;
@@ -707,7 +707,7 @@ var App = {
                             if(!found) skipMotherTreeLots =  true;
                         } else {
                             App.Utility.log(`\t\tPlanted ${selectedPlant.plantId} (${selectedPlant.plant.stats.type})!`);
-                            await App.Farm.Plant.add("0", selectedPlant);
+                            await App.Farm.Plant.add(selectedFarm, "0", selectedPlant);
                         }
                         motherTree_available--;
                     }
@@ -876,16 +876,18 @@ var App = {
                     }));
                 });
             },
-            add: function(land, plant) {
+            add: function(farm, land, plant) {
                 return new Promise(async function(resolve) {
                     await App.Utility.timeout();
                     if(!isNaN(plant)) {
                         resolve(await App.Request.post(App.Constant.ROOT_URL + App.Constant.API.FARMS, {
+                            farmId: farm._id,
                             landId: land,
                             sunflowerId: plant
                         }));
                     } else {
                         resolve(await App.Request.post(App.Constant.ROOT_URL + App.Constant.API.FARMS, {
+                            farmId: farm._id,
                             landId: land,
                             plantId: plant.plantId
                         }));
